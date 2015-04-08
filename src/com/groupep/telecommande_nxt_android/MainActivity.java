cecb4154902e0d7f79bc6b671d5a782b838328d7
InternationalKoder <international_koder@yahoo.com>
2015-04-08 09:37:44 +0200

    Ajout du bluetooth qui fontionne mal

--- src/com/groupep/telecommande_nxt_android/MainActivity.java
+++ src/com/groupep/telecommande_nxt_android/MainActivity.java
@@ -1,27 +1,37 @@
 package com.groupep.telecommande_nxt_android;
 
+import java.io.IOException;
+import java.util.UUID;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
+	private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 	private Button boutonConnecter, boutonDeconnecter, boutonSortieMilieu, boutonSortieDernier;
+	private EditText champNomRobot;
 	private RadioButton radioAvancer, radioReculer;
 	private SeekBar vitesseAvancer, vitesseReculer;
-	private BluetoothAdapter connexionRobot;
+	private BluetoothAdapter connexionBluetooth;
+	private BluetoothSocket connexionRobot;
+	private TextView texteNomRobotConnecte;
 	
 
 	@Override
@@ -30,14 +40,19 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(bluetoothReceiver, filter);
+		
 		this.boutonConnecter = (Button) findViewById(R.id.boutonConnecter);
 		this.boutonDeconnecter = (Button) findViewById(R.id.boutonDeconnecter);
 		this.boutonSortieMilieu = (Button) findViewById(R.id.boutonSortieMilieu);
 		this.boutonSortieDernier = (Button) findViewById(R.id.boutonSortieDernier);
+		this.champNomRobot = (EditText) findViewById(R.id.champNomRobot);
 		this.radioAvancer = (RadioButton) findViewById(R.id.radioAvancer);
 		this.radioReculer = (RadioButton) findViewById(R.id.radioReculer);
 		this.vitesseAvancer = (SeekBar) findViewById(R.id.vitesseAvancer);
 		this.vitesseReculer = (SeekBar) findViewById(R.id.vitesseReculer);
+		this.texteNomRobotConnecte = (TextView) findViewById(R.id.texteNomRobotConnecte);
 		
 		this.setEnabled(false);
 		
@@ -48,31 +63,56 @@ public class MainActivity extends Activity
 			public void onClick(View v)
 			{
 				// TODO : connexion bluetooth
-				/*connexionRobot = BluetoothAdapter.getDefaultAdapter();
-				if(connexionRobot == null)
-					Toast.makeText(MainActivity.this, "Pas de bluetooth", Toast.LENGTH_SHORT).show();
-				else
+				// Activation du bluetooth
+				connexionBluetooth = BluetoothAdapter.getDefaultAdapter();
+				if(!connexionBluetooth.isEnabled())
 				{
-					if (!connexionRobot.isEnabled())
-						Toast.makeText(MainActivity.this, "Vous devez activer le bluetooth pour continuer", Toast.LENGTH_SHORT).show();
-
-					else
-					{
-						private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver()
-						{
-							public void onReceive(Context context, Intent intent)
-							{
-								String action = intent.getAction();
-								if(BluetoothDevice.ACTION_FOUND.equals(action))
-								{
-								    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-								    Toast.makeText(TutoBluetoothActivity.this, "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
-								}
-							}
-						};
-					}
-				}*/
+					Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			        startActivityForResult(turnOn, 0);
+			        Toast.makeText(getApplicationContext(),"Bluetooth activé", Toast.LENGTH_SHORT).show();
+			    }
+			    else
+			        Toast.makeText(getApplicationContext(),"Bluetooth déjà activé", Toast.LENGTH_SHORT).show();
 				
+				// Recherche du robot dans les périphériques connus
+				for(BluetoothDevice dev : connexionBluetooth.getBondedDevices())
+				{
+					if(dev.getName().equals(champNomRobot.getText().toString()))
+					{
+						try
+						{
+							connexionRobot = dev.createRfcommSocketToServiceRecord(MY_UUID);
+						}
+						catch (IOException e)
+						{
+							Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+						}
+					}
+				}
+				
+				// Recherche du robot si non trouvé dans les périphériques connus
+				if(connexionRobot == null)
+					connexionBluetooth.startDiscovery();
+				
+				if(connexionRobot != null)
+				{
+					String nom = connexionRobot.getRemoteDevice().getName();
+					texteNomRobotConnecte.setText(nom);
+					Toast.makeText(MainActivity.this, "Connecté à " + nom, Toast.LENGTH_SHORT).show();
+					try
+					{
+						connexionRobot.getOutputStream().write(0);
+					}
+					catch (IOException e)
+					{
+						Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+					}
+				}
+				else
+					Toast.makeText(MainActivity.this, "Robot non trouvé", Toast.LENGTH_SHORT).show();
+					
+				
+				// affichage
 				setEnabled(true);
 				vitesseReculer.setEnabled(false);
 				radioAvancer.setChecked(true);
@@ -86,6 +126,20 @@ public class MainActivity extends Activity
 			public void onClick(View v)
 			{
 				// TODO : déconnexion bluetooth
+				if(connexionBluetooth != null)
+				{
+					try
+					{
+						if(connexionRobot != null)
+							connexionRobot.close();
+					}
+					catch (IOException e)
+					{
+						Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+					}
+					connexionBluetooth.disable();
+					Toast.makeText(MainActivity.this, "Bluetooth désactivé", Toast.LENGTH_SHORT).show();
+				}
 				
 				setEnabled(false);
 			}
@@ -106,9 +160,18 @@ public class MainActivity extends Activity
 		this.vitesseAvancer.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
 		{
 			@Override
-			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2)
+			public void onProgressChanged(SeekBar arg0, int val, boolean arg2)
 			{
 				// TODO : Envoi de la vitesse
+				try
+				{
+					if(connexionRobot != null)
+						connexionRobot.getOutputStream().write(val);
+				}
+				catch(IOException e)
+				{
+					Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+				}
 			}
 
 			@Override
@@ -119,6 +182,71 @@ public class MainActivity extends Activity
 			@Override
 			public void onStopTrackingTouch(SeekBar arg0)
 			{
+			}
+		});
+		
+		
+		this.vitesseReculer.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+		{
+			@Override
+			public void onProgressChanged(SeekBar arg0, int val, boolean arg2)
+			{
+				// TODO : Envoi de la vitesse
+				try
+				{
+					if(connexionRobot != null)
+						connexionRobot.getOutputStream().write(val * -1);
+				}
+				catch(IOException e)
+				{
+					Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0)
+			{
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0)
+			{
+			}
+		});
+		
+		
+		this.boutonSortieMilieu.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				try
+				{
+					if(connexionRobot != null)
+						connexionRobot.getOutputStream().write(127);
+				}
+				catch (IOException e)
+				{
+					Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+		
+		
+		this.boutonSortieDernier.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				try
+				{
+					if(connexionRobot != null)
+						connexionRobot.getOutputStream().write(-127);
+				}
+				catch (IOException e)
+				{
+					Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
 	}
@@ -134,5 +262,39 @@ public class MainActivity extends Activity
 		this.radioReculer.setEnabled(enabled);
 		this.vitesseAvancer.setEnabled(enabled);
 		this.vitesseReculer.setEnabled(enabled);
+	}
+	
+	
+	private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver()
+	{
+		public void onReceive(Context context, Intent intent)
+		{
+			String action = intent.getAction();
+			if(BluetoothDevice.ACTION_FOUND.equals(action))
+			{
+				BluetoothDevice dev = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+				Toast.makeText(MainActivity.this, "dev : " + dev.getName(), Toast.LENGTH_SHORT).show();
+			  
+				if(dev.getName().equals(champNomRobot.getText().toString()))
+				{
+					try
+					{
+						connexionRobot = dev.createRfcommSocketToServiceRecord(MY_UUID);
+					}
+					catch (IOException e)
+					{
+						Toast.makeText(MainActivity.this, "Erreur de communication", Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+		}
+	};
+	
+	
+	protected void onDestroy()
+	{
+		  super.onDestroy();
+		  connexionBluetooth.cancelDiscovery();
+		  unregisterReceiver(bluetoothReceiver);
 	}
 }
